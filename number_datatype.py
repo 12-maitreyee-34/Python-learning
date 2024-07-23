fd0e94dbaa4bab9803dec53630bd045f01ef29e9
Maitreyee Bhosale <115869337+12-maitreyee-34@users.noreply.github.com>
2024-07-23 05:16:26 -0700

    Add files via upload

---
+++ number_datatype.py
@@ -0,0 +1,30 @@
+#Datatype and operations
+
+#Defining a integer,float datatype
+a=9
+b=9.5
+c=int(input("enter the number:"))
+
+#Arithmetic Operations
+#addition
+result=a+b  
+print(result)
+print(type(result))
+
+#subtraction
+print(a-b)
+
+#multiplication
+print(a*b)
+
+#division
+print(a/c)
+
+#mod operator
+print(a%c)
+
+#power operator
+print(a**c)
+
+#floor division
+print(a//c)
